10323bcbfe220231acfaa0128783410dba5eba4a
Abdelrazek-Alshaal <abdelrazkalshaal@gmail.com>
2025-08-20 05:07:55 +0300

    Add portfolio files

---
+++ java t1.js
@@ -0,0 +1,7 @@
+// Future enhancements like scroll animations or dark mode toggle can go here
+console.log("Welcome to Abdelrazek's Portfolio!");
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  }
+  
